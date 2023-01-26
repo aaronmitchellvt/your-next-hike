eb51c4be752329977f18267c40adea4cc1db23d5
aaron mitchell <aaronmitchell9902@gmail.com>
2023-01-25 22:51:27 -0500

    styling auth pages

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -6,32 +6,32 @@ const Signup = () => {
   return (
     <Layout
       pageContent={
-        <div className="grid place-items-center mt-16">
-          <div className="bg-white w-4/5 w:md-1/3 h-auto rounded p-6">
-            <h1 className="text-black text-xl text-center font-bold pt-3">
-              Create an account
-            </h1>
-            <button
-              className="p-2 mt-3 w-full bg-blue-500 text-white rounded"
-              onClick={() => useGoogleSignIn()}
-            >
-              <span className="flex justify-between items-center pr-3">
-                <p className="pl-3">Contine with Google</p>
-                <ion-icon name="logo-google"></ion-icon>
-              </span>
-            </button>
-            <div className="flex justify-center py-3">
-              <span className="flex items-center">
-                <p>Already have an account?</p>
-                <Link href="/auth/login">
-                  <p className="pl-2 text-green-700 font-bold underline">
-                    Log in
-                  </p>
-                </Link>
-              </span>
-            </div>
+        <div className="grid place-items-center mt-16 pt-8">
+        <div className="bg-white w-3/5 w:md-1/3  h-auto rounded p-6">
+          <h1 className="text-black text-xl text-center font-bold pt-3">
+            Create an account
+          </h1>
+          <button
+            className="p-2 mt-3 w-full bg-orange-500 hover:bg-orange-600 text-white rounded"
+            onClick={() => useGoogleSignIn()}
+          >
+            <span className="flex justify-between items-center pr-3">
+              <p className="pl-3">Contine with Google</p>
+              <ion-icon name="logo-google"></ion-icon>
+            </span>
+          </button>
+          <div className="flex justify-center py-3">
+            <span className="flex items-center">
+              <p>Already have an account?</p>
+              <Link href="/auth/login">
+                <p className="pl-2 text-green-700 hover:text-green-800 font-bold underline">
+                  Sign in
+                </p>
+              </Link>
+            </span>
           </div>
         </div>
+      </div>
       }
     ></Layout>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,9 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>My NEXT Hike</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <script
-          type="module"
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-        ></script>
-        <script
-          noModule
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-        ></script>
-      </Head>
       <Layout
         pageContent={
-          <div className="text-black">index page</div>
+          <div className="text-black mt-16">index page</div>
         }
       ></Layout>
     </>
